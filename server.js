bc6a311b1b124ccef0d9c649d6906f020867f934
Will <whof@bu.edu>
2021-07-01 15:27:33 -0400

    removed server.js and package.json

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const mail = require("nodemailer");
-const PORT = process.env.port || 8000
-app.use(express.static("./client"))
-app.listen(PORT,function(){
-    console.log(`http://localhost:${PORT}`)
-})
